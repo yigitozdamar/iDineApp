06157a598f22d2f2bbe6961b3b87c9d203f11321
yigit <yigitozdamar@hotmail.com>
2022-08-28 20:06:15 +0300

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Yigit Ozdamar on 28.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
